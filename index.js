c1ca5d37fca76bbff7fe5515e576a4e547ea860e
baggette <58569691+Baggette@users.noreply.github.com>
2022-07-17 23:35:32 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const { Client, Intents, Message} = require('discord.js');
+const util = require('minecraft-server-util');
+const {MessageEmbed, MessageSelectMenu} = require('discord.js');
+const options = {
+    timeout: 1000 * 5, // timeout in milliseconds
+    enableSRV: true // SRV record lookup
+};
+const prefix = "!"; //prefix
+const client = new Client({
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES
+    ]
+});
+client.on('ready', () => {
+    console.log('bot started');// so you know the bot started
+    //if you don't want the activity status comment out the line below using //
+    client.user.setPresence({ activities: [{ name: `${server_ip}`, type: 'WATCHING' }], status: 'active' });// just a little feature to display whatever server is under server_ip
+});
+const server_ip = "exampleserver.net"; //replace with real ip for the server
+const server_port = 12345; // replace with the real port also thanks dashcruft for testing the code
+client.on('messageCreate', (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+    util.status(server_ip, server_port, options)
+    .then((result) => {
+        
+        const string1 = JSON.stringify(result);// turn the object into a string
+        const string = JSON.parse(string1);// make the string parsable
+        const embed = new MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("example server status")
+    .setDescription(`This will show the status and info about the minecraft server \n **Server ip:** ${server_ip} \n **Server port:** ${server_port}`)
+    .addFields(
+        {name:"Server Version", value: `${string.version.name}`},
+        {name:"Server Protocol Version", value:`${string.version.protocol}`},
+        {name:"Players Online", value:`${string.players.online}`},
+        {name:"Max Players", value:`${string.players.max}`},
+        {name:"MOTD (May Not Display Accurately)", value:`${string.motd.clean}`},
+        {name:"Latency", value:`${string.roundTripLatency}`},
+    )
+    .setTimestamp()
+    message.channel.send({embeds: [embed]})// send the embed
+    })
+    
+    .catch((error) => console.error(error));
+
+
+});
+client.login("1234567890abcdefghijk")//replace with your bot token
+//you may need to give the bot the privileged gateway intents
+//this was inspired by this gist https://gist.github.com/vegeta897/e3d2fcd4b661bd9d021c397e7505be05 (this is outdated, do not use it)
